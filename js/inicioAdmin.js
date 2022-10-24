7e6cc78c47aafcb92911814e72be851024bfa370
RicardoAyalaColli <115853665+RicardoAyalaColli@users.noreply.github.com>
2022-10-24 11:02:38 -0500

    Add files via upload

---
+++ js/inicioAdmin.js
@@ -0,0 +1,23 @@
+/*document.querySelector(".dropdown").addEventListener("click",function(){
+    document.querySelector(".dropdown ul").classList.add("show");
+})*/
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropMenu')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
---
+++ js/jaja.js
@@ -0,0 +1,3 @@
+document.querySelector(".dropdown").addEventListener("click",function(){
+    document.querySelector(".dropdown ul").classList.add("show");
+})
